f4e1f131b630bfddd7169e315538d1931b2fb35a
Joben Apao <jobenapao01>
2023-05-22 16:49:28 +0800

    Skills section edited

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -78,7 +78,7 @@ const Works = () => {
 	return (
 		<>
 			<motion.div variants={textVariant()}>
-				<p className={styles.sectionSubText}>My Work</p>
+				<p className={styles.sectionSubText}>My Works</p>
 				<h2 className={styles.sectionHeadText}>Projects.</h2>
 			</motion.div>
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -43,6 +43,10 @@ export const navLinks = [
 		title: 'Work',
 	},
 	{
+		id: 'skills',
+		title: 'Skills',
+	},
+	{
 		id: 'projects',
 		title: 'Projects',
 	},
